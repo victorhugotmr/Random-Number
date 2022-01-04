2b1b3354a13b141e7a5f06bc46d0a25e618333d3
Victor Hugo <torugoestudos@gmail.com>
2022-01-04 17:02:28 -0300

    include media queries and error message

---
+++ README.md
@@ -0,0 +1,7 @@
+This project was created to study some Javascript functions and proprieties.
+
+New funcionalities will be added like:
+
+- Choose odds or even numbers;
+- Repeat or not a number;
+- A container which shows the previous numbers; 
--- app.js
+++ app.js
@@ -5,7 +5,8 @@ let minNumber = document.querySelector("#minNumber");
 let maxNumber = document.querySelector("#maxNumber");
 let btn = document.querySelector("#button");
 let result = document.querySelector("#result");
-let error = document.querySelector("#error")
+let error = document.querySelector(".error");
+let closeBtn = document.querySelector("#close");
 
 // Creating functions
 
@@ -18,19 +19,31 @@ function randomize(){
   let minimum = Number(minNumber.value);
   let maximum = Number(maxNumber.value);
 
-  if (maximum <= minimum) {
-    console.log("error")
+  if (maximum <= minimum) { 
+    
+  error.style.display = "flex";
+
+  console.log("error");
 
   } else {
     const randomNumber = (min, max) =>
     Math.floor(Math.random() * (max - min + 1)) + min;
 
     result.innerHTML = `<h1>${randomNumber(minimum, maximum)}</h1>`;
+
     
   }
 }
 
+function closeError(){
+  error.style.display = "none";
+  maxNumber.value = "10";
+}
+
+window.onload = closeError();
+
 // Assigning the functions
 
 form.addEventListener('submit', preventDefault);
+closeBtn.addEventListener('click', closeError)
 btn.addEventListener('click', randomize);
